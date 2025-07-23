e0359e3fb86cfec6da3acda79fd3c33e52732865
Muhammad Umar Javed <muja00003@stud.uni-saarland.de>
2025-07-23 09:28:38 +0200

    fix?

--- src/open_clip_train/main.py
+++ src/open_clip_train/main.py
@@ -222,7 +222,6 @@ def main(args):
         model_kwargs['init_logit_bias'] = -10
     if args.use_adversary:
         model_kwargs['use_adversary'] = args.use_adversary
-        model_kwargs['adversarial_loss_weight'] = args.adversarial_loss_weight
     model, preprocess_train, preprocess_val = create_model_and_transforms(
         args.model,
         args.pretrained,
